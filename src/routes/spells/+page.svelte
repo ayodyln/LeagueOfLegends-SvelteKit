961dec664853ae919b7a7a8b52bb6687d3620f87
ayodyln <78442693+ayodyln@users.noreply.github.com>
2023-04-26 12:20:57 -0600

    summoners page started

--- src/routes/spells/+page.svelte
+++ src/routes/spells/+page.svelte
@@ -1,3 +1,27 @@
-<script></script>
+<script lang="ts">
+	import { onMount } from 'svelte'
+	// http://ddragon.leagueoflegends.com/cdn/13.8.1/img/spell/SummonerFlash.png
 
-<main class="w-full max-w-5xl m-auto flex-grow p-4" />
+	let spells: any = []
+
+	onMount(async () => {
+		try {
+			const summoners = await fetch(
+				'http://ddragon.leagueoflegends.com/cdn/13.8.1/data/en_US/summoner.json'
+			)
+			const res = await summoners.json()
+			console.log(res)
+			spells = Object.values(res.data).map((spell) => spell)
+		} catch (error) {
+			console.error(error)
+		}
+	})
+</script>
+
+<main class="w-full max-w-5xl m-auto flex-grow p-4">
+	{#each spells as spell}
+		<div>
+			<button>{spell.name}</button>
+		</div>
+	{/each}
+</main>
