b772ef8e45731c1a45bea1d554d2f5f0e5ad1574
ayodyln <78442693+ayodyln@users.noreply.github.com>
2023-05-01 16:22:52 -0600

    fixed null error for favorites storage

--- src/lib/stores/index.ts
+++ src/lib/stores/index.ts
@@ -19,6 +19,7 @@ const initFavorites = browser ? window.localStorage.getItem('favorites') : ['Aat
 export const favorites: any = writable(initFavorites)
 
 favorites.subscribe((value: any) => {
+	if (value === null) value = []
 	if (browser) window.localStorage.setItem('favorites', value)
 })
 
